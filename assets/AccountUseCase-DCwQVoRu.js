e6ee7f7440e006db29965c3342809b18fa60605e
paulMatencio <paulo.matencio@gmail.com>
2026-05-23 14:58:37 +0200

    Updates

--- assets/AccountUseCase-DCwQVoRu.js
+++
@@ -1,1 +0,0 @@
-import{a0 as a}from"./index-CEJYAiCN.js";class c{constructor(r){this.authRepo=r}async upgradeAccount(r,t){try{return await this.authRepo.updateUser({email:r,password:t})}catch(o){throw a.fromError(o)}}async updatePassword(r){try{return await this.authRepo.updateUser({password:r})}catch(t){throw a.fromError(t)}}}export{c as AccountUseCase};
